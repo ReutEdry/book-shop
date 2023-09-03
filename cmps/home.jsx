a383c091c24b28dbaf845415bdeea97b879a9c0a
reut <reutedry1@gmail.com>
2023-09-03 18:08:37 +0300

    starting

--- cmps/home.jsx
+++ cmps/home.jsx
@@ -3,8 +3,8 @@ import { UserPreview } from './user-preview.jsx'
 export function Home() {
     return (
         <section>
-            <h2>Home Sweet Home</h2>
-            <UserPreview />
+            <h2>Book store</h2>
+            {/* <UserPreview /> */}
         </section>
     )
 }
--- root-cmp.jsx
+++ root-cmp.jsx
@@ -3,7 +3,7 @@ import { Home } from './cmps/home.jsx'
 export function App() {
     return <section className="app">
         <header className="app-header">
-            <h1>My App</h1>
+            <h1>Welcom to my Book store</h1>
         </header>
         <main className="container">
             <Home />
